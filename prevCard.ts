81624f516e489b4657b594e5e170afe4ddefe2cc
Atul Kumar Awasthi <53579888+kumaratul60@users.noreply.github.com>
2024-09-22 19:34:22 +0530

    Create prevCard.ts

---
+++ prevCard.ts
@@ -0,0 +1,1 @@
+<a href="https://app.daily.dev/atuldev12"><img align="right" src="https://api.daily.dev/devcards/99f2714dded04f30b399dbc50b7fe41c.png?r=ts4" width="300" alt="Atul Awasthi's Dev Card"/></a>
