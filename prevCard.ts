5d29876cdf93b6e364cd2a867bbc2420652f1fce
Atul Kumar Awasthi <53579888+kumaratul60@users.noreply.github.com>
2024-09-22 20:31:35 +0530

    Update prevCard.ts

--- prevCard.ts
+++ prevCard.ts
@@ -1,1 +1,105 @@
 <a href="https://app.daily.dev/atuldev12"><img align="right" src="https://api.daily.dev/devcards/99f2714dded04f30b399dbc50b7fe41c.png?r=ts4" width="300" alt="Atul Awasthi's Dev Card"/></a>
+
+  
+[<img height="30" src="https://img.shields.io/badge/twitter-%231DA1F2.svg?&style=flat&logo=twitter&logoColor=white" />][twitter]
+[<img height="30" src="https://img.shields.io/badge/linkedin-3.7K+-blue.svg?&style=flat&logo=linkedin&logoColor=white" />][LinkedIn]
+[<img height="30" src="https://img.shields.io/badge/medium-black.svg?&style=plastic&logo=medium&logoColor=white" />][medium]
+[<img height="30" src="https://img.shields.io/badge/hackerrank-brightgreen.svg?&style=flat&logo=hackerrank&logoColor=white" />][hackerrank]
+[<img height="30" src="https://img.shields.io/badge/leetcode-yellow.svg?&style=square&logo=leetcode&logoColor=white" />][leetcode]
+[<img height="30" src="https://img.shields.io/badge/geeksforgeeks-brightgreen.svg?&style=plastic&logo=geeksforgeeks&logoColor=white" />][geeksforgeeks]
+
+<a href="https://app.daily.dev/atuldev12"><img align="right" src="https://api.daily.dev/devcards/v2/SnC6qUoCM.png?type=default&r=gkg" width="280" alt="Atul Awasthi's Dev Card"/></a>
+
+ ### <img src="https://media.giphy.com/media/VgCDAzcKvsR6OM0uWg/giphy.gif" width="50"> A little more about me...
+ 
+- 💻 I'm a curious self-taught developer.
+- 📍  I'm from India
+- 🕸️ I am passionate about building websites with React.js & Next.js.
+- 🤝  I like to meet new people.
+- 🌋 I’m always looking for challenging work opportunities ahead.
+- ⚡ Fun fact: I am always learning and trying new things || I love CrossFit games.
+- 💡 ![Lines of code](https://img.shields.io/badge/From%20Hello%20World%20I%27ve%20Written-1.5%20million+%20lines%20of%20code-blue)
+- ✅ Ask me any thing [here](https://github.com/kumaratul60/kumaratul60/issues/new), or [email](atulreso1@gmail.com) me.
+
+
+<img src="https://media.giphy.com/media/LnQjpWaON8nhr21vNW/giphy.gif" width="60"> <em><b>I love connecting with different people</b> so if you want to say <b>hi, I'll be happy to meet you more!</b> 😊 </em>
+
+## ⚡ Technologies
+
+### Language :
+
+![JavaScript](https://img.shields.io/badge/-JavaScript-black?style=flat-square&logo=javascript)
+![HTML5](https://img.shields.io/badge/-HTML5-E34F26?style=flat-square&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/-CSS3-1572B6?style=flat-square&logo=css3)
+![TypeScript](https://img.shields.io/badge/-TypeScript-007ACC?style=flat-square&logo=typescript)
+![MySQL](https://img.shields.io/badge/-MySQL-black?style=flat-square&logo=mysql)
+![Java](https://img.shields.io/badge/-java-E34A86?style=flat-square&logo=openjdk)
+
+
+### Libraries & Framework :
+
+![React](https://img.shields.io/badge/-React-black?style=flat-square&logo=react)
+![Nodejs](https://img.shields.io/badge/-Nodejs-black?style=flat-square&logo=Node.js)
+![Express JS](https://img.shields.io/badge/-Expressjs-black?style=flat-square&logo=Express.js)
+![Mongoose](https://img.shields.io/badge/-Mongoose-black?style=flat-square&logo=Mongoose)
+![JWT](https://img.shields.io/badge/-JWT-black?style=flat-square&logo=jwt)
+<a href="#"><img alt="MongoDB" src ="https://img.shields.io/badge/MongoDB-%234ea94b.svg?logo=mongodb&logoColor=white"></a>
+
+
+![Redis](https://img.shields.io/badge/-Redis-black?style=flat-square&logo=Redis)
+![Bootstrap](https://img.shields.io/badge/-Bootstrap-563D7C?style=flat-square&logo=bootstrap)
+<a href="#"><img alt="Material Design" src="https://img.shields.io/badge/Material%20Design%20-%230081CB.svg?logo=material-design&logoColor=white"></a>
+![Jest](https://img.shields.io/badge/-jest-BC3B14?style=flat-square&logo=Jest)
+
+
+### DevOps & Cloud Tools:
+
+<a href="#"><img alt="GitHub Pages" src="https://img.shields.io/badge/GitHub%20Pages-%23327FC7.svg?logo=github&logoColor=white"></a>
+![Amazon AWS](https://img.shields.io/badge/Amazon%20AWS-232F3E?style=flat-square&logo=amazon-aws)
+![Git](https://img.shields.io/badge/-Git-black?style=flat-square&logo=git)
+<a href="#"><img alt="Postman" src="https://img.shields.io/badge/Postman-FF6C37?logo=postman&logoColor=white"></a>
+<a href="#"><img alt="Vercel" src="https://img.shields.io/badge/Vercel%20-%23000000.svg?logo=vercel&logoColor=white"></a>
+<a href="#"><img alt="Netlify" src="https://img.shields.io/badge/Netlify%20-%23000000.svg?logo=netlify&logoColor=white"></a>
+
+## 💰 Support
+<p>
+<a href='https://www.buymeacoffee.com/atulkawasthi' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png' border='0' alt='Support Atul on buymecoffee' /></a>
+</p>
+
+
+## 📊 Github Stats📈
+ 
+<p align="center">
+ <img width="40%" src="https://github-readme-stats.vercel.app/api?username=kumaratul60&show_icons=true&theme=dark&title_color=ff8000&text_color=ffffff&bg_color=6a6a6a&locale=en&hide_border=true" alt="kumaratul60" />
+<img width="30%" src="https://github-readme-stats.vercel.app/api/top-langs?username=kumaratul60&show_icons=true&theme=dark&title_color=ff8000&text_color=ffffff&bg_color=6a6a6a&locale=en&layout=compact&hide_border=true" alt="kumaratul60" /> 
+  
+<img width="42%" src="https://github-readme-streak-stats.herokuapp.com/?user=kumaratul60&theme=highcontrast&hide_border=true" alt="kumaratul60" />
+<img width="28%" src="http://github-profile-summary-cards.vercel.app/api/cards/productive-time?username=kumaratul60&theme=highcontrast&utcOffset=8&hide_border=true" alt="kumaratul60" />
+</p>
+
+
+
+[![Atul's github activity graph](https://github-readme-activity-graph.vercel.app/graph?username=kumaratul60&bg_color=0f2d3d&color=1cadfb&line=1cadfb&point=1cadfb&area=true&hide_border=true)](https://github.com/kumaratul60/github-readme-activity-graph)
+
+🌱😍 Profile Visits 
+
+ ![Visitors](https://profile-counter.glitch.me/{kumaratul60}/count.svg?align=right)
+
+ 📶 Hire me
+- :paperclip: [My Portfolio](https://atultheportfolio.netlify.app)
+- :email: atulreso1@gmail.com
+
+ 
+ [Twitter]:https://twitter.com/atulkawasthi
+ [linkedin]:https://www.linkedin.com/in/atul-kumar-awasthi
+ [medium]:https://medium.com/@atulkawasthi
+ [hackerrank]:https://www.hackerrank.com/atulreso1
+ [gmail]: https://gmail.com
+ [leetcode]: https://leetcode.com/atulreso1
+ [geeksforgeeks]: https://auth.geeksforgeeks.org/user/atul0063
+ 
+ 
+"Code is easy and available everywhere, but knowing what code to write, where to write it, and how to write it well is what makes a developer known as the best." 
+
+###  [![Typing SVG](https://readme-typing-svg.herokuapp.com?size=24&width=600&lines=+Always+happy+to+Help+;)](https://git.io/typing-svg)
+
